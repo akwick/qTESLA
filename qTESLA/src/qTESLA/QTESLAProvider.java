996cd19da9544c037a6427afc6e9a6cfcf88c369
ue17apyt <alexandergreat524@gmail.com>
2019-01-22 02:32:03 +0100

    Add Provider Class

---
+++ qTESLA/src/qTESLA/QTESLAProvider.java
@@ -0,0 +1,21 @@
+package qTESLA;
+
+import java.security.Provider;
+
+public class QTESLAProvider extends Provider {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8348304362615727658L;
+	
+	protected QTESLAProvider() {
+		
+		super ("qTESLAProvider", 2.3, "qTESLA Provider 2.3, An Efficient and Post-Quantum Secure Lattice-Based Signature Scheme");
+		
+		put ("KeyPairGenerator.QTESLAKeyPairGenerator", QTESLAKeyPairGenerator.class.getName());
+		put ("Signature.QTESLASignature", QTESLASignature.class.getName());
+	
+	}
+	
+}
